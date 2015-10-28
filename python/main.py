7dda02cf2c099e0fa9dc6a1b950c10653f424f6f
Steve Johnson <sjohnson0684@gmail.com>
2015-10-28 10:06:53 -0400

    Added config name format

--- python/main.py
+++ python/main.py
@@ -4,6 +4,7 @@ from config import generate_config
 # settings
 output_directory_name = "output"
 scales_supported = [1, 2, 3]
+config_name_format = "HarderSolarSystem-%dscale"
 
 # directories
 script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -12,4 +13,5 @@ output_directory = os.path.join(base_directory, output_directory_name)
 
 # generate configs
 for scale in scales_supported:
-	generate_config(scale=scale, directory=output_directory)
+	config_name = config_name_format % scale
+	generate_config(scale=scale, directory=output_directory, name=config_name)
