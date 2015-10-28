400a887ea892ceb419ad5dbb94023c9e35ea3a8d
Steve Johnson <sjohnson0684@gmail.com>
2015-10-28 10:21:11 -0400

    Changing config to mod
    We are generating an entire mod, not just a config.

--- python/main.py
+++ python/main.py
@@ -1,10 +1,9 @@
 import os
-from config import generate_config
+from mod import generate_mod
 
 # settings
 output_directory_name = "output"
 scales_supported = [1, 2, 3]
-config_name_format = "HarderSolarSystem-%dscale"
 
 # directories
 script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -13,5 +12,4 @@ output_directory = os.path.join(base_directory, output_directory_name)
 
 # generate configs
 for scale in scales_supported:
-	config_name = config_name_format % scale
-	generate_config(scale=scale, directory=output_directory, name=config_name)
+	generate_mod(scale=scale, directory=output_directory)
