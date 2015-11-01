3f86f09af4d24ca8a5b7410dcfe86958aec95673
Steve Johnson <sjohnson0684@gmail.com>
2015-11-01 06:55:19 -0500

    Support for adding surface gravity changes.

--- python/mod.py
+++ python/mod.py
@@ -125,6 +125,8 @@ def generate_opm_compatibility_config(scale, compatibility_path):
 			properties_module.add_parameter("@radius = %d" % round(body.r))
 			if not body.is_tidally_locked:
 				properties_module.add_parameter("@rotationPeriod = %s" % format_float(body.rot))
+		if body.gee_ASL is not None:
+			properties_module.add_parameter("geeASL = %s" % format_float(body.gee_ASL))
 		if not properties_module.is_empty:
 			body_module.add_child(properties_module)
 		
@@ -181,6 +183,8 @@ def generate_kopernicus_config(scale, mod_path):
 				else:
 					rot_speed = body.rot
 				properties_module.add_parameter("rotationPeriod = %s" % format_float(rot_speed))
+		if body.gee_ASL is not None:
+			properties_module.add_parameter("geeASL = %s" % format_float(body.gee_ASL))
 		if not properties_module.is_empty:
 			body_module.add_child(properties_module)
 		
